e86105fbc080fd1c9accc407a0129686724b7f20
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-11-14 20:17:40 +0000

    Fix semantic tags

--- src/features/header/CategoriesNav.tsx
+++ src/features/header/CategoriesNav.tsx
@@ -3,35 +3,41 @@ import NavigationLink from "../../ui/NavigationLink";
 
 export default function CategoriesNav() {
   return (
-    <nav className="h-12 bg-slate-700">
-      <ul className="flex items-center justify-center gap-4 py-1 text-lg text-slate-200">
-        <NavigationLink
-          to="products"
-          title="All products"
-          onClick={() => console.log("a")}
-        />
-        <NavigationLink
-          to="electronics"
-          title="Electronics"
-          onClick={() => console.log("a")}
-        />
-        <NavigationLink
-          to="jewelery"
-          title="Jewelery"
-          onClick={() => console.log("a")}
-        />
-        <NavigationLink
-          to="men's-clothing"
-          title="Men's clothing"
-          onClick={() => console.log("a")}
-        />
-        <NavigationLink
-          to="women's-clothing"
-          title="Women's clothing"
-          onClick={() => console.log("a")}
-        />
-      </ul>
-      <Outlet />
-    </nav>
+    <>
+      <header>
+        <nav className="h-12 bg-slate-700">
+          <ul className="flex items-center justify-center gap-4 py-1 text-lg text-slate-200">
+            <NavigationLink
+              to="products"
+              title="All products"
+              onClick={() => console.log("a")}
+            />
+            <NavigationLink
+              to="electronics"
+              title="Electronics"
+              onClick={() => console.log("a")}
+            />
+            <NavigationLink
+              to="jewelery"
+              title="Jewelery"
+              onClick={() => console.log("a")}
+            />
+            <NavigationLink
+              to="men's-clothing"
+              title="Men's clothing"
+              onClick={() => console.log("a")}
+            />
+            <NavigationLink
+              to="women's-clothing"
+              title="Women's clothing"
+              onClick={() => console.log("a")}
+            />
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 }
