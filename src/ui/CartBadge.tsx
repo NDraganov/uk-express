e1b22dc4b30dfb8d4813ffeda3b4106a90ad1b8a
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-11-23 14:15:40 +0000

    Conditionally render the checkout button if there are items in the cart

--- src/ui/CartBadge.tsx
+++ src/ui/CartBadge.tsx
@@ -7,6 +7,7 @@ interface CartBadgeProps {
 }
 
 export default function CartBadge({ onOpen }: CartBadgeProps) {
+  const cartItems = useAppSelector((state) => state.cart.items);
   const cartQuantity = useAppSelector((state) =>
     state.cart.items.reduce((val, item) => val + item.quantity, 0),
   );
@@ -16,7 +17,7 @@ export default function CartBadge({ onOpen }: CartBadgeProps) {
       <button className="flex items-center h-full" onClick={onOpen}>
         <BsCart4 />{" "}
         <span className="absolute top-1 right-4 text-orange-500 rounded-full">
-          {cartQuantity}
+          {cartItems.length > 0 && cartQuantity}
         </span>
       </button>
     </IconContext.Provider>
--- src/ui/CheckoutButton.tsx
+++ src/ui/CheckoutButton.tsx
@@ -1,9 +1,16 @@
 import { NavLink } from "react-router-dom";
+import { useAppSelector } from "../store/hooks";
 
 export default function CheckoutButton() {
+  const cartItems = useAppSelector((state) => state.cart.items);
+
   return (
-    <button className="rounded-full ring-1 ring-slate-700 px-5 py-1 mt-2 mb-2 hover:ring-offset-1 hover:ring-orange-500 text-slate-700">
-      <NavLink to="/checkout">Checkout</NavLink>
-    </button>
+    <div>
+      {cartItems.length > 0 && (
+        <button className="rounded-full ring-1 ring-slate-700 px-5 py-1 mt-2 mb-2 hover:ring-offset-1 hover:ring-orange-500 text-slate-700">
+          <NavLink to="/checkout">Checkout</NavLink>
+        </button>
+      )}
+    </div>
   );
 }
