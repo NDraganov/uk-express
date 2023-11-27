0150c7d0726e2b455a455c3e09ccfd749fb5dd52
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-11-27 18:39:46 +0000

    Prepare the signup form

--- src/features/auth/register/SignupForm.tsx
+++ src/features/auth/register/SignupForm.tsx
@@ -1,14 +1,28 @@
 import { useForm } from "react-hook-form";
 import SubmitButton from "../../../ui/SubmitButton";
 
-export default function RegisterForm() {
-  const { register, handleSubmit } = useForm();
+export default function SignupForm() {
+  const { register, getValues, handleSubmit } = useForm();
+
+  const onSubmit = handleSubmit((data) => console.log(data));
 
   return (
     <form
       className="flex items-center justify-center flex-col gap-4 w-1/5"
-      onSubmit={handleSubmit}
+      onSubmit={onSubmit}
     >
+      <div className="flex items-start justify-center flex-col w-full">
+        <label className="text-lg text-slate-700 font-light" htmlFor="fullName">
+          Full name
+        </label>
+        <input
+          className="w-full border border-slate-300 rounded-md focus:outline-none focus:border-orange-500 focus:ring-orange-500 placeholder-slate-400 my-1"
+          type="text"
+          placeholder="Full name"
+          {...register("fullName", { required: "This field is required!" })}
+          required
+        />
+      </div>
       <div className="flex items-start justify-center flex-col w-full">
         <label className="text-lg text-slate-700 font-light" htmlFor="email">
           Email
@@ -17,7 +31,13 @@ export default function RegisterForm() {
           className="w-full border border-slate-300 rounded-md focus:outline-none focus:border-orange-500 focus:ring-orange-500 placeholder-slate-400 my-1"
           type="email"
           placeholder="Email"
-          {...register("email")}
+          {...register("email", {
+            required: "This field is required!",
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "Please provide a valid email address!",
+            },
+          })}
           required
         />
       </div>
@@ -29,7 +49,13 @@ export default function RegisterForm() {
           className="w-full border border-slate-300 rounded-md focus:outline-none focus:border-orange-500 focus:ring-orange-500 placeholder-slate-400 my-1"
           type="password"
           placeholder="Password"
-          {...register("password")}
+          {...register("password", {
+            required: "This field is required!",
+            minLength: {
+              value: 8,
+              message: "Password needs a minimum of 8 characters!",
+            },
+          })}
           required
         />
       </div>
@@ -44,7 +70,11 @@ export default function RegisterForm() {
           className="w-full border border-slate-300 rounded-md focus:outline-none focus:border-orange-500 focus:ring-orange-500 placeholder-slate-400 my-1"
           type="password"
           placeholder="Confirm password"
-          {...register("confirmPassword")}
+          {...register("confirmPassword", {
+            required: "This field is required!",
+            validate: (value) =>
+              value === getValues().password || "Passwords need to match!",
+          })}
           required
         />
       </div>
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,11 +1,11 @@
-import RegisterForm from "../features/auth/register/RegisterForm";
+import SignupForm from "../features/auth/register/SignupForm";
 
 export default function RegisterPage() {
   return (
     <main>
       <h1>Reagister Page</h1>
       <div className="flex items-center justify-center h-screen">
-        <RegisterForm />
+        <SignupForm />
       </div>
     </main>
   );
