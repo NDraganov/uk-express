201f2444b4189dc5e5102859a9d60af1d9ca02c3
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-11-30 01:35:15 +0000

    Add cart details and the checkout button to my cart

--- src/features/user-account/sidebar-links/my-cart/MyCart.tsx
+++ src/features/user-account/sidebar-links/my-cart/MyCart.tsx
@@ -1,7 +1,12 @@
+import CartDetails from "../../../checkout/CartDetails";
+import CheckoutButton from "../../../../ui/CheckoutButton";
+
 export default function MyCart() {
   return (
-    <div>
+    <div className="flex h-full flex-col items-center justify-center">
       <h1>MY CARD</h1>
+      <CartDetails />
+      <CheckoutButton />
     </div>
   );
 }
