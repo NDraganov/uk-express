65babbe48e08c9b21b03a5b04c2ef845f3a24541
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-12-01 01:34:57 +0000

    Add the sign out functionality to user page

--- src/features/user-account/Sidebar.tsx
+++ src/features/user-account/Sidebar.tsx
@@ -1,6 +1,14 @@
+import { useAppDispatch } from "../../store/hooks";
 import SidebarLink from "../../ui/SidebarLink";
+import { signOutUser } from "../auth/authSlice";
 
 export default function Sidebar() {
+  const dispatch = useAppDispatch();
+
+  function onSignOut() {
+    dispatch(signOutUser());
+  }
+
   return (
     <aside className="min-h-screen w-1/4 border-r-2 border-gray-200">
       <nav className="h-full">
@@ -8,7 +16,7 @@ export default function Sidebar() {
           <SidebarLink to="account-information" title="ACCOUNT INFORMATION" />
           <SidebarLink to="my-card" title="MY CART" />
           <SidebarLink to="change-password" title="CHANGE PASSWORD" />
-          <SidebarLink to="/" title="SIGN OUT" />
+          <SidebarLink to="/" title="SIGN OUT" onClick={onSignOut} />
         </ul>
       </nav>
     </aside>
