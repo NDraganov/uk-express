8b775eb0dc1a2cd7e37e8d6202264a5da5b9b584
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-12-02 18:02:22 +0000

    Delete the old categories and reducers from the store

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,6 @@ import Header from "./features/header/Header";
 import CategoriesNav from "./features/header/CategoriesNav";
 import HomePage from "./pages/HomePage";
 import AllProducts from "./features/categories/all-products/AllProducts";
-import Electronics from "./features/categories/electronics/Electronics";
-import Jewelry from "./features/categories/jewelry/Jewelry";
-import WomenClothing from "./features/categories/women-clothing/WomenClothing";
-import MenClothing from "./features/categories/men-clothing/MensClothing";
 import ProductPage from "./pages/ProductPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import OrderConfirmationPage from "./pages/OrderConfirmationPage";
@@ -29,20 +25,37 @@ export default function App() {
       <CategoriesNav />
       <Routes>
         <Route index element={<HomePage />} />
+        // All products Route
         <Route path="/products" element={<AllProducts />} />
+        // Categories routes
         <Route
           path="/products/category/smartphones"
           element={<Smartphones />}
         />
         <Route path="/products/category/laptops" element={<Laptops />} />
+        <Route path="/products/category/fragrances" element={} />
+        <Route path="/products/category/skincare" element={} />
+        <Route path="/products/category/groceries" element={} />
+        <Route path="/products/category/home-decoration" element={} />
+        <Route path="/products/category/furniture" element={} />
+        <Route path="/products/category/tops" element={} />
+        <Route path="/products/category/sunglasses" element={} />
+        <Route path="/products/category/automotive" element={} />
+        <Route path="/products/category/motorcycle" element={} />
+        <Route path="/products/category/womens-dresses" element={} />
+        <Route path="/products/category/womens-shoes" element={} />
+        <Route path="/products/category/womens-watches" element={} />
+        <Route path="/products/category/womens-bags" element={} />
+        <Route path="/products/category/womens-jewellery" element={} />
+        <Route path="/products/category/mens-shirts" element={} />
+        <Route path="/products/category/mens-shoes" element={} />
+        <Route path="/products/category/mens-watches" element={} />
+        // Singe product Route
         <Route
           path="/all-products/product/:productId"
           element={<ProductPage />}
         />
-        <Route path="/electronics" element={<Electronics />} />
-        <Route path="/jewelry" element={<Jewelry />} />
-        <Route path="/men-clothing" element={<MenClothing />} />
-        <Route path="/women-clothing" element={<WomenClothing />} />
+        // User Route
         <Route element={<ProtectedRoute />}>
           <Route path="/users/:userId" element={<UserAccountPage />}>
             <Route
@@ -55,12 +68,14 @@ export default function App() {
               element={<ChangePassword />}
             />
           </Route>
+          // Checkout Route
           <Route path="/checkout" element={<CheckoutPage />} />
           <Route
             path="/order-confirmation"
             element={<OrderConfirmationPage />}
           />
         </Route>
+        // Auth Routes
         <Route path="/sign-in" element={<SignInPage />} />
         <Route path="/sign-up" element={<RegisterPage />} />
         <Route path="*" element={<PageNotFound />} />
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../features/cart/cartSlice";
-import electronicsReducer from "../features/categories/electronics/electronicsSlice";
-import jewelryReducer from "../features/categories/jewelry/jewelerySlice";
-import mensReducer from "../features/categories/men-clothing/menSlice";
-import womanReducer from "../features/categories/women-clothing/womenSlice";
 import authReducer from "../features/auth/authSlice";
 import { productsApi } from "../api/productsApiSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    electronics: electronicsReducer,
-    jewelery: jewelryReducer,
-    menClothing: mensReducer,
-    womanClothing: womanReducer,
     cart: cartReducer,
     [productsApi.reducerPath]: productsApi.reducer,
   },
