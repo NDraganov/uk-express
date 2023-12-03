f37aded5e4fdf5486c15cec898ef2744a5199a9a
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-12-03 13:34:22 +0000

    Remove brand and stock from product item

--- src/ui/ProductItem.tsx
+++ src/ui/ProductItem.tsx
@@ -6,12 +6,11 @@ import Button from "./Button";
 
 export default function ProductItem({
   id,
-  brand,
+
   title,
   price,
   images,
   rating,
-  stock,
 }: Product) {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
@@ -28,16 +27,12 @@ export default function ProductItem({
     <div className="flex h-full flex-col items-center justify-between rounded-md border border-gray-300 p-2 text-slate-700 shadow-lg shadow-gray-300">
       <img className="w-full rounded-t-md" src={images[1]} alt={title} />
       <div className=" w-full">
-        <h3 className="text-xl">{title}</h3>
-        <h4>{brand}</h4>
+        <p className="text-xl">{title}</p>
         <p className="my-2">
           ⭐️⭐️⭐️⭐️⭐️ <span>{rating}</span>
         </p>
         <p>
           £<span className="text-xl font-medium">{price}</span>
-        </p>
-        <p>
-          availability: <span className="font-normal">{stock}</span>
         </p>
         <div className="flex justify-between gap-4">
           <Button title="Review" onClick={handleReviewProduct} />
--- src/ui/ProductsList.tsx
+++ src/ui/ProductsList.tsx
@@ -17,7 +17,7 @@ export default function ProductsList({ data, isLoading }: ProductListProps) {
   return (
     <div className="mx-40 my-20">
       <p className="mb-4 text-2xl font-medium text-slate-700">Results</p>
-      <ul className="grid gap-20 md:grid-cols-3">
+      <ul className="grid gap-5 md:grid-cols-4">
         {data?.map((product) => (
           <li key={product.id}>
             <ProductItem {...product} />
