33525caa083d42c16eb7ba7695d48d492b157473
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-12-04 20:18:31 +0000

    Add dark mode to the product list and the related components

--- src/ui/ProductItem.tsx
+++ src/ui/ProductItem.tsx
@@ -24,15 +24,18 @@ export default function ProductItem({
   }
 
   return (
-    <div className="flex h-full flex-col items-center justify-between rounded-md border border-gray-300 p-2 text-slate-700 shadow-lg shadow-gray-300">
-      <img className="w-full rounded-t-md" src={images[1]} alt={title} />
+    <div className="flex h-full flex-col items-center justify-between rounded-md border border-gray-300 p-2 shadow-lg shadow-gray-300 dark:border-gray-500 dark:bg-slate-800 dark:shadow-none">
+      <img className="h-1/2 w-full rounded-t-md" src={images[1]} alt={title} />
       <div className=" w-full">
         <p className="text-xl">{title}</p>
         <p className="my-2">
           ⭐️⭐️⭐️⭐️⭐️ <span>{rating}</span>
         </p>
-        <p>
-          £<span className="text-xl font-medium">{price}</span>
+        <p className="font-light">
+          £
+          <span className="text-xl font-normal dark:text-cyan-500">
+            {price}
+          </span>
         </p>
         <div className="flex justify-between gap-4">
           <Button title="Review" onClick={handleReviewProduct} />
