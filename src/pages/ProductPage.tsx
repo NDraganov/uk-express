f71d7f5bbc4c442e550353074af8c18bbe599008
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-12-05 14:07:15 +0000

    Add dark mode for the description of the product page

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -12,38 +12,46 @@ export default function ProductPage() {
   );
 
   return (
-    <main className="my-40">
+    <main className="mx-20 my-40">
       {!product && <p>Error</p>}
-      <div className="ml-10 mt-10">
-        <BackButton />
-      </div>
-      <div className="my-10 flex">
-        <div className="w-1/2">
+
+      <BackButton />
+
+      <div className="my-10 flex gap-4">
+        <section className="w-1/2">
           <ProductGallery id={productId} />
-        </div>
-        <section className="flex w-1/2 flex-col items-baseline justify-start pt-10">
-          <h2 className="text-3xl text-slate-700">{product?.title}</h2>
-          <p>{product?.brand}</p>
-          <p className="pt-10 text-slate-600">{product?.description}</p>
-          <div className="justify-left flex w-full flex-col items-baseline">
-            <p className="pt-5">
-              ⭐️⭐️⭐️⭐️⭐️
-              <span className="text-slate-700"> {product?.rating}</span>
-            </p>
-            <p className="pt-5">
-              <span className="text-2xl font-semibold text-slate-700">
-                £{product?.price}
-              </span>
-            </p>
-            <p className="text-slate-700">
-              availability:
-              <span> {product?.stock}</span>
-            </p>
-          </div>
-          <div className="flex w-full items-center justify-end gap-4">
-            <CheckoutButton />
-          </div>
         </section>
+
+        <section className="flex w-1/2 flex-col items-baseline justify-start">
+          <h2 className="text-3xl font-extralight dark:text-white">
+            {product?.title}
+          </h2>
+
+          <p>{product?.brand}</p>
+
+          <p className="pt-5">
+            ⭐️⭐️⭐️⭐️⭐️
+            <span className="dark:text-white"> {product?.rating}</span>
+          </p>
+
+          <p className="pt-5">
+            <span className="text-xl font-semibold dark:text-white">
+              £<span className="text-2xl">{product?.price}</span>
+            </span>
+          </p>
+          <p>
+            Availability :
+            <span className="dark:text-white">
+              {" "}
+              {product?.stock === 0 ? "Out of stock" : "In Stock"}
+            </span>
+          </p>
+
+          <p className="pt-5">{product?.description}</p>
+        </section>
+      </div>
+      <div className="flex w-full items-center justify-end gap-4">
+        <CheckoutButton />
       </div>
     </main>
   );
