9c2c98b7908cee3506bc9031b7e35fa95f65f1f2
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-12-07 15:14:00 +0000

    Change the styles of the product item

--- src/features/product/product/ProductItem.tsx
+++ src/features/product/product/ProductItem.tsx
@@ -27,15 +27,12 @@ export default function ProductItem({
     <div className="flex h-full flex-col items-center justify-between rounded-md border border-gray-300 p-2 shadow-lg shadow-gray-300 dark:border-gray-500 dark:bg-slate-800 dark:shadow-none">
       <img className="h-1/2 w-full rounded-t-md" src={thumbnail} alt={title} />
       <div className=" w-full">
-        <p className="text-xl">{title}</p>
+        <p className="text-xl dark:text-white">{title}</p>
         <p className="my-2">
           ⭐️⭐️⭐️⭐️⭐️ <span>{rating}</span>
         </p>
-        <p className="font-light">
-          £
-          <span className="text-xl font-normal dark:text-cyan-500">
-            {price}
-          </span>
+        <p className="text-xl font-light dark:text-cyan-500">
+          £<span className="font-normal">{price}</span>
         </p>
         <div className="flex justify-between gap-4">
           <Button title="Review" onClick={handleReviewProduct} />
