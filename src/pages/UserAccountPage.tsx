97f38be9d82152c9be432369933af31b3b037bf3
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-12-07 15:19:03 +0000

    Add more margin for the user account page because of the taller navigation

--- src/pages/UserAccountPage.tsx
+++ src/pages/UserAccountPage.tsx
@@ -3,7 +3,7 @@ import Sidebar from "../features/user-account/Sidebar";
 
 export default function UserAccountPage() {
   return (
-    <div className="mt-20 min-h-screen">
+    <div className="mt-28 min-h-screen">
       <div className="flex">
         <Sidebar />
         <main className="min-h-screen w-full">
