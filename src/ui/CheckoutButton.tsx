d123df0b713f3f5192fcd535aafe0609672ab3d3
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-12-07 15:22:16 +0000

    Add bounce animation to the checkout button

--- src/ui/CheckoutButton.tsx
+++ src/ui/CheckoutButton.tsx
@@ -14,7 +14,7 @@ export default function CheckoutButton({ title }: CheckoutButtonProps) {
     <div>
       {cartItems.length > 0 && (
         <button
-          className="mb-2 mt-2 rounded-full px-5 py-1 ring-1 ring-black hover:ring-slate-500 hover:ring-offset-1 hover:ring-offset-slate-500 dark:text-white dark:ring-cyan-500 dark:hover:text-cyan-300 dark:hover:ring-cyan-300 dark:hover:ring-offset-gray-800"
+          className="mb-2 mt-2 animate-bounce rounded-full px-5 py-1 ring-1 ring-black hover:ring-slate-500 hover:ring-offset-1 hover:ring-offset-slate-500 dark:text-white dark:ring-cyan-500 dark:hover:text-cyan-300 dark:hover:ring-cyan-300 dark:hover:ring-offset-gray-800"
           onClick={() => dispatch(closeCart())}
         >
           <NavLink to="/checkout">{title}</NavLink>
