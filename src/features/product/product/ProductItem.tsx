79397dd006d52275d5f50c3fd7b2f5d26ccb2f30
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-12-07 15:04:38 +0000

    Set fixed height to product item image for better appearance

--- src/features/product/product/ProductItem.tsx
+++ src/features/product/product/ProductItem.tsx
@@ -25,7 +25,7 @@ export default function ProductItem({
 
   return (
     <div className="flex h-full flex-col items-center justify-between rounded-md border border-gray-300 p-2 shadow-lg shadow-gray-300 dark:border-gray-500 dark:bg-slate-800 dark:shadow-none">
-      <img className="w-full rounded-t-md" src={thumbnail} alt={title} />
+      <img className="h-1/2 w-full rounded-t-md" src={thumbnail} alt={title} />
       <div className=" w-full">
         <p className="text-xl">{title}</p>
         <p className="my-2">
