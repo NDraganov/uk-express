8e5c50f4a19272726927094e97646e16b11dbb71
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-12-09 22:51:55 +0000

    Add state for the overflow to prevent scrolling when the modal is open

--- src/features/news-subscription/newsSlice.ts
+++ src/features/news-subscription/newsSlice.ts
@@ -2,10 +2,12 @@ import { createSlice } from "@reduxjs/toolkit";
 
 interface News {
   isOpen: boolean;
+  overflow: string;
 }
 
 const initialState: News = {
   isOpen: false,
+  overflow: document.body.style.overflow,
 };
 
 const newsSlice = createSlice({
@@ -14,9 +16,11 @@ const newsSlice = createSlice({
   reducers: {
     openModalTimer(state) {
       state.isOpen = true;
+      state.overflow = document.body.style.overflow = "hidden";
     },
     closeModal(state) {
       state.isOpen = false;
+      state.overflow = document.body.style.overflow = "unset";
     },
   },
 });
