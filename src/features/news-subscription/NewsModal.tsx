1cd258bc0a775806da4539187807ed23328a88fc
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-12-10 00:19:38 +0000

    Add text, input and close button with styles to the modal

--- src/features/news-subscription/NewsModal.tsx
+++ src/features/news-subscription/NewsModal.tsx
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { closeModal, openModalTimer } from "./newsSlice";
+import Icon from "../../ui/Icon";
+import { AiOutlineClose } from "react-icons/ai";
+import { FiSend } from "react-icons/fi";
 
 export default function NewsModal() {
   const { isOpen } = useAppSelector((state) => state.news);
@@ -19,11 +22,41 @@ export default function NewsModal() {
   return (
     <>
       {isOpen && (
-        <div className="absolute bottom-0 left-0 right-0 top-0 z-10 flex scroll-m-0 items-center justify-center bg-white/60">
-          <div className="text-3xl text-red-600">
-            <h1>Modal</h1>
-            <p>vygbuhvyctvygbhnjhbgvyftcdrtfvygbhnj</p>
-            <button onClick={() => dispatch(closeModal())}>Close</button>
+        <div className="absolute bottom-0 left-0 right-0 top-0 z-10 flex scroll-m-0 items-center justify-center bg-white/50">
+          <div className="flex h-96 w-[40rem] flex-col rounded-md bg-violet-950 p-4 text-white">
+            <div className="flex items-center justify-end">
+              <button onClick={() => dispatch(closeModal())}>
+                <Icon
+                  value={{
+                    className: "text-2xl fill-slate-400 hover:fill-white",
+                  }}
+                >
+                  <AiOutlineClose />
+                </Icon>
+              </button>
+            </div>
+
+            <div className="flex flex-col items-start justify-center px-10">
+              <h2 className="py-4 text-[4rem] font-extrabold leading-none">
+                Never miss out on new products!
+              </h2>
+              <p className="pb-6 pr-28 text-lg text-indigo-300">
+                Subscribe for our newsletter to be inform whenever we offer new
+                products!
+              </p>
+              <div className="flex items-center rounded-md px-2 ring-4 ring-pink-500 ">
+                <input
+                  className="rounded-l-md border-0 bg-violet-950 placeholder:text-white focus:ring-0"
+                  type="email"
+                  placeholder="Email"
+                />
+                <div>
+                  <Icon value={{ className: "text-xl" }}>
+                    <FiSend />
+                  </Icon>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       )}
