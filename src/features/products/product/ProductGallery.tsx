7ce1c24c3a0fc39c3a031fd96e8f303a9b74a078
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-12-12 21:53:39 +0000

    Fix border opacity with adding divs around image as coloured border

--- src/features/products/product/ProductGallery.tsx
+++ src/features/products/product/ProductGallery.tsx
@@ -7,10 +7,12 @@ interface ProductGalleryProps {
 
 export default function ProductGallery({ id }: ProductGalleryProps) {
   const [imageIndex, setImageIndex] = useState(0);
-  const { data } = useGetAllProductsQuery(undefined);
-  const product = data?.products.find((product) => product.id === Number(id));
+  const { data: products } = useGetAllProductsQuery(undefined);
+  const product = products?.products.find(
+    (product) => product.id === Number(id),
+  );
 
-  function handleSetIndex(index: number) {
+  function handleImageIndex(index: number) {
     setImageIndex(index);
   }
 
@@ -18,24 +20,30 @@ export default function ProductGallery({ id }: ProductGalleryProps) {
     <div className="flex flex-col gap-4">
       <div className="h-3/5">
         <img
-          className="h-96 w-full rounded-md border-2 border-gray-400 dark:border-gray-500"
+          className="h-96 w-full rounded-md"
           src={product?.images[imageIndex]}
-          alt=""
+          alt={product?.title}
         />
       </div>
       <div className="grid h-2/5 grid-flow-col place-items-center gap-2">
         {product?.images.map((image, index) => (
-          <img
-            className={`h-32 w-40 rounded-md border-4 hover:cursor-pointer hover:opacity-60 ${
-              imageIndex === index
-                ? "border-orange-500 opacity-60 dark:border-cyan-500"
-                : "border-white dark:border-slate-900"
+          <div
+            className={`rounded-md p-[0.125rem] ${
+              imageIndex === index ? "bg-orange-500 dark:bg-cyan-500" : ""
             }`}
-            key={image}
-            src={image}
-            alt="image"
-            onClick={() => handleSetIndex(index)}
-          />
+          >
+            <div className="m-0 rounded-md bg-white p-0">
+              <img
+                className={`h-32 w-40 rounded-md hover:cursor-pointer hover:opacity-60 ${
+                  imageIndex === index ? "opacity-60" : ""
+                }`}
+                key={image}
+                src={image}
+                alt={product.title}
+                onClick={() => handleImageIndex(index)}
+              />
+            </div>
+          </div>
         ))}
       </div>
     </div>
