8e25ca62bf7a31296530496ac9ecdbca3b379e1c
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-12-13 02:07:48 +0000

    Add product rating to the product item and improve the styles

--- src/features/products/product/ProductItem.tsx
+++ src/features/products/product/ProductItem.tsx
@@ -2,6 +2,7 @@ import { useAppDispatch } from "../../../store/hooks";
 import { useNavigate } from "react-router-dom";
 import { addToCart } from "../../cart/cartSlice";
 import { type Product } from "../../../api/products-types/productsTypes";
+import ProductRating from "./ProductRating";
 import Button from "../../../ui/Button";
 
 export default function ProductItem({
@@ -15,14 +16,6 @@ export default function ProductItem({
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
-  function handleAddToCart() {
-    dispatch(addToCart({ id, title, price, thumbnail, images }));
-  }
-
-  function handleReviewProduct() {
-    navigate(`/products/${id}`);
-  }
-
   return (
     <div className="flex h-[28rem] flex-col items-center rounded-md border border-gray-300 p-2 shadow-lg shadow-gray-300 dark:border-gray-500 dark:bg-slate-800 dark:shadow-none">
       <div className="h-3/5 w-full">
@@ -32,17 +25,29 @@ export default function ProductItem({
           alt={title}
         />
       </div>
-      <div className="flex h-2/5 w-full flex-col justify-between">
-        <p className="text-xl dark:text-white">{title}</p>
-        <p className="my-2">
-          ⭐️⭐️⭐️⭐️⭐️ <span>{rating}</span>
-        </p>
-        <p className="text-xl font-light dark:text-cyan-500">
+
+      <div className="flex h-2/5 w-full flex-col justify-between pt-2">
+        <p className="text-lg font-normal dark:text-gray-300">{title}</p>
+
+        <div className="my-2 flex items-center gap-1">
+          <ProductRating rating={rating.toFixed(0)} />
+          <p className="text-gray-500 dark:text-gray-400">
+            <span>{rating.toFixed(0)}</span>
+          </p>
+        </div>
+
+        <p className="text-xl font-light dark:text-gray-300">
           £<span className="font-normal">{price}</span>
         </p>
+
         <div className="flex justify-between">
-          <Button title="Review" onClick={handleReviewProduct} />
-          <Button title="Add to cart" onClick={handleAddToCart} />
+          <Button title="Review" onClick={() => navigate(`/products/${id}`)} />
+          <Button
+            title="Add to cart"
+            onClick={() =>
+              dispatch(addToCart({ id, title, price, thumbnail, images }))
+            }
+          />
         </div>
       </div>
     </div>
