b9955577dfc8292ba0cb9a09429dfd9e406b1081
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-12-14 12:12:31 +0000

    Add styles to the toast

--- src/ui/PushNotification.tsx
+++ src/ui/PushNotification.tsx
@@ -8,10 +8,10 @@ export default function PushNotification() {
   return (
     <div className="fixed right-40 top-32 ">
       <Toast>
-        <div>
+        <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-md bg-lime-300 text-lg text-lime-600 dark:bg-cyan-700 dark:text-cyan-200">
           <MdOutlineAddShoppingCart />
         </div>
-        <div className="text-lg text-red-600">Product added to cart!</div>
+        <div className="mx-3 text-base">Product added to cart!</div>
         <Toast.Toggle onClick={() => dispatch(closeToast())} />
       </Toast>
     </div>
