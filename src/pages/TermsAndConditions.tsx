74ac49c61487209a3565e4b4d5e51a543a21ed8d
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-12-17 01:53:22 +0000

    Add styles to the terms page

--- src/pages/TermsAndConditions.tsx
+++ src/pages/TermsAndConditions.tsx
@@ -1,136 +1,147 @@
 export default function TermsAndContions() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">
+    <div className="mx-80 mb-10 mt-40">
+      <h1 className="text-3xl font-bold dark:text-white">
         UkExpress Terms and Conditions Policy
       </h1>
-      <p>
-        Welcome to UkExpress These terms and conditions outline the rules and
-        regulations for the use of (Store Name)’s Website.
-      </p>
-      <p className="font-semibold">
-        UkExpress is located at: ,{" "}
-        <address>Wembley Park 306, London, UK</address>.
-      </p>
+      <div>
+        <p className="py-4 font-light">
+          Welcome to UkExpress These terms and conditions outline the rules and
+          regulations for the use of (Store Name)’s Website.
+        </p>
+        <p className="flex gap-2 py-2 font-semibold dark:text-gray-300">
+          UkExpress is located at:{" "}
+          <address>Wembley Park 306, London, UK</address>
+        </p>
+      </div>
 
-      <p>
-        By accessing this website we assume you accept these terms and
-        conditions in full. Do not continue to use (Store Name)’s website if you
-        do not accept all of the terms and conditions stated on this page.
-      </p>
-      <p>
-        The following terminology applies to these Terms and Conditions, Privacy
-        Statement and Disclaimer Notice and any or all Agreements: Client, You
-        and Your refers to you, the person accessing this website and accepting
-        the Company’s terms and conditions. The Company, Ourselves, We, Our and
-        Us, refers to our Company. Party, Parties, or Us, refers to both the
-        Client and ourselves, or either the Client or ourselves.
-      </p>
+      <div>
+        <p className="py-4 font-light">
+          By accessing this website we assume you accept these terms and
+          conditions in full. Do not continue to use (Store Name)’s website if
+          you do not accept all of the terms and conditions stated on this page.
+        </p>
+        <p className="py-4 font-light">
+          The following terminology applies to these Terms and Conditions,
+          Privacy Statement and Disclaimer Notice and any or all Agreements:
+          Client, You and Your refers to you, the person accessing this website
+          and accepting the Company’s terms and conditions. The Company,
+          Ourselves, We, Our and Us, refers to our Company. Party, Parties, or
+          Us, refers to both the Client and ourselves, or either the Client or
+          ourselves.
+        </p>
+        <p className="py-4 font-light">
+          All terms refer to the offer, acceptance and consideration of payment
+          necessary to undertake the process of our assistance to the Client in
+          the most appropriate manner, whether by formal meetings of a fixed
+          duration, or any other means, for the express purpose of meeting the
+          Client’s needs in respect of provision of the Company’s stated
+          services/products, in accordance with and subject to, prevailing law
+          of (Address).
+        </p>
+        <p className="py-4 font-light">
+          Any use of the above terminology or other words in the singular,
+          plural, capitalisation and/or he/she or they, are taken as
+          interchangeable and therefore as referring to same.
+        </p>
+      </div>
 
-      <p>
-        All terms refer to the offer, acceptance and consideration of payment
-        necessary to undertake the process of our assistance to the Client in
-        the most appropriate manner, whether by formal meetings of a fixed
-        duration, or any other means, for the express purpose of meeting the
-        Client’s needs in respect of provision of the Company’s stated
-        services/products, in accordance with and subject to, prevailing law of
-        (Address).
-      </p>
+      <div>
+        <h2 className="py-2 font-semibold dark:text-white">Cookies</h2>
 
-      <p>
-        Any use of the above terminology or other words in the singular, plural,
-        capitalisation and/or he/she or they, are taken as interchangeable and
-        therefore as referring to same.
-      </p>
+        <p className="py-4 font-light">
+          We employ the use of cookies. By using (Store Name)’s website you
+          consent to the use of cookies in accordance with (Store Name)’s
+          privacy policy. Most of the modern day interactive websites use
+          cookies to enable us to retrieve user details for each visit.
+        </p>
+        <p className="py-4 font-light">
+          Cookies are used in some areas of our site to enable the functionality
+          of this area and ease of use for those people visiting. Some of our
+          affiliate / advertising partners may also use cookies.
+        </p>
+      </div>
 
-      <h2>Cookies</h2>
+      <div>
+        <h2 className="py-2 font-semibold dark:text-white">License</h2>
+        <p className="py-4 font-light">
+          Unless otherwise stated, (Store Name) and/or its licensors own the
+          intellectual property rights for all material on (Store Name).
+        </p>
 
-      <p>
-        We employ the use of cookies. By using (Store Name)’s website you
-        consent to the use of cookies in accordance with (Store Name)’s privacy
-        policy. Most of the modern day interactive websites use cookies to
-        enable us to retrieve user details for each visit.
-      </p>
-      <p>
-        Cookies are used in some areas of our site to enable the functionality
-        of this area and ease of use for those people visiting. Some of our
-        affiliate / advertising partners may also use cookies.
-      </p>
+        <p className="py-4 font-light">
+          All intellectual property rights are reserved. You may view and/or
+          print pages from (Add URL) for your own personal use subject to
+          restrictions set in these terms and conditions.
+        </p>
 
-      <h2>License</h2>
-      <p>
-        Unless otherwise stated, (Store Name) and/or its licensors own the
-        intellectual property rights for all material on (Store Name).
-      </p>
+        <p className="py-4 dark:text-gray-300">You must not:</p>
+        <ul className="list-disc py-2 pl-7 font-light">
+          <li>Republish material from (Add URL).</li>
+          <li>Sell, rent or sub-license material from (Add URL).</li>
+          <li>Reproduce, duplicate or copy material from (Add URL).</li>
+          <li>
+            Redistribute content from (Store Name) (unless content is
+            specifically made for redistribution).
+          </li>
+        </ul>
+      </div>
 
-      <p>
-        All intellectual property rights are reserved. You may view and/or print
-        pages from (Add URL) for your own personal use subject to restrictions
-        set in these terms and conditions.
-      </p>
+      <div>
+        <h2 className="py-2 font-semibold dark:text-white">Disclaimer</h2>
 
-      <p>You must not:</p>
-      <ul>
-        <li>Republish material from (Add URL).</li>
-        <li>Sell, rent or sub-license material from (Add URL).</li>
-        <li>Reproduce, duplicate or copy material from (Add URL).</li>
-        <li>
-          Redistribute content from (Store Name) (unless content is specifically
-          made for redistribution).
-        </li>
-      </ul>
+        <p className="py-4 font-light">
+          To the maximum extent permitted by applicable law, we exclude all
+          representations, warranties and conditions relating to our website and
+          the use of this website (including, without limitation, any warranties
+          implied by law in respect of satisfactory quality, fitness for purpose
+          and/or the use of reasonable care and skill).
+        </p>
 
-      <h2>Disclaimer</h2>
+        <p className="py-4 dark:text-gray-300">
+          Nothing in this disclaimer will:
+        </p>
 
-      <p>
-        To the maximum extent permitted by applicable law, we exclude all
-        representations, warranties and conditions relating to our website and
-        the use of this website (including, without limitation, any warranties
-        implied by law in respect of satisfactory quality, fitness for purpose
-        and/or the use of reasonable care and skill).
-      </p>
+        <ul className="list-disc py-2 pl-7 font-light">
+          <li>
+            Limit or exclude our or your liability for death or personal injury
+            resulting from negligence.
+          </li>
+          <li>
+            Limit or exclude our or your liability for fraud or fraudulent
+            misrepresentation.
+          </li>
+          <li>
+            Limit any of our or your liabilities in any way that is not
+            permitted under applicable law.
+          </li>
+          <li>
+            Or exclude any of our or your liabilities that may not be excluded
+            under applicable law.
+          </li>
+        </ul>
 
-      <p>Nothing in this disclaimer will:</p>
+        <p className="py-4 dark:text-gray-300">
+          The limitations and exclusions of liability set out in this Section
+          and elsewhere in this disclaimer:
+        </p>
 
-      <ul>
-        <li>
-          Limit or exclude our or your liability for death or personal injury
-          resulting from negligence.
-        </li>
-        <li>
-          Limit or exclude our or your liability for fraud or fraudulent
-          misrepresentation.
-        </li>
-        <li>
-          Limit any of our or your liabilities in any way that is not permitted
-          under applicable law.
-        </li>
-        <li>
-          Or exclude any of our or your liabilities that may not be excluded
-          under applicable law.
-        </li>
-      </ul>
+        <ul className="list-disc py-2 pl-7 font-light">
+          <li>are subject to the preceding paragraph; and</li>
+          <li>
+            govern all liabilities arising under the disclaimer or in relation
+            to the subject matter of this disclaimer, including liabilities that
+            arise in contract, tort (including negligence) and for breach of
+            statutory duty.
+          </li>
+        </ul>
 
-      <p>
-        The limitations and exclusions of liability set out in this Section and
-        elsewhere in this disclaimer:
-      </p>
-
-      <ul>
-        <li>are subject to the preceding paragraph; and</li>
-        <li>
-          govern all liabilities arising under the disclaimer or in relation to
-          the subject matter of this disclaimer, including liabilities that
-          arise in contract, tort (including negligence) and for breach of
-          statutory duty.
-        </li>
-      </ul>
-
-      <p>
-        To the extent that the website and the information and services on the
-        website are provided free of charge, we will not be liable for any loss
-        or damage of any nature.
-      </p>
+        <p className="py-4 font-light">
+          To the extent that the website and the information and services on the
+          website are provided free of charge, we will not be liable for any
+          loss or damage of any nature.
+        </p>
+      </div>
     </div>
   );
 }
