a979773e162ab5428f76fecaf888127db1e49bf9
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2023-12-25 23:10:05 +0000

    Add styles to the policies navigation
    Add styles for the active state of the policy link. Add icons to the links

--- src/features/policies/PoliciesNav.tsx
+++ src/features/policies/PoliciesNav.tsx
@@ -1,13 +1,19 @@
 import PolicyNavLink from "./PolicyNavLink";
+import { IoDocumentTextSharp } from "react-icons/io5";
+import { MdPrivacyTip } from "react-icons/md";
 
 export default function PoliciesNav() {
   return (
-    <div className="flex flex-col items-center pt-4">
-      <h1 className="text-3xl">UkExpress Policies</h1>
+    <div className="flex flex-col items-center border border-l-0 border-r-0 border-t-0 border-slate-400 pt-4 dark:border-gray-500">
+      <h1 className="text-3xl dark:text-white">UkExpress Policies</h1>
       <nav className="mt-4">
-        <ul className="flex items-center gap-4">
-          <PolicyNavLink title="Terms and Conditions" to="terms&conditions" />
-          <PolicyNavLink title="Privacy" to="privacy" />
+        <ul className="flex items-center">
+          <PolicyNavLink
+            title="Terms & Conditions"
+            to="terms&conditions"
+            icon={<IoDocumentTextSharp />}
+          />
+          <PolicyNavLink title="Privacy" to="privacy" icon={<MdPrivacyTip />} />
         </ul>
       </nav>
     </div>
--- src/features/policies/PolicyNavLink.tsx
+++ src/features/policies/PolicyNavLink.tsx
@@ -1,14 +1,39 @@
-import { NavLink } from "react-router-dom";
+import { type ReactNode } from "react";
+import { NavLink, NavLinkProps } from "react-router-dom";
+import Icon from "../../ui/Icon";
 
-interface PolicyNavLinkProps {
+type PolicyNavLinkProps = {
   title: string;
   to: string;
-}
+  icon: ReactNode;
+} & NavLinkProps;
 
-export default function PolicyNavLink({ title, to }: PolicyNavLinkProps) {
+export default function PolicyNavLink({
+  title,
+  to,
+  icon,
+  ...restProps
+}: PolicyNavLinkProps) {
   return (
-    <NavLink to={to}>
-      <li>{title}</li>
+    <NavLink
+      className={({ isActive }) => (isActive ? "text-lg " : "text-lg")}
+      to={to}
+      {...restProps}
+    >
+      {({ isActive }) => (
+        <li
+          className={`flex items-center justify-center gap-2 rounded-t-xl px-4 pt-2 hover:bg-zinc-50 dark:hover:bg-slate-700 ${
+            isActive
+              ? "bg-zinc-100 dark:bg-slate-800 dark:text-cyan-500"
+              : "dark:text-gray-300"
+          }`}
+        >
+          <Icon value={{ className: isActive ? "fill-cyan-600" : "" }}>
+            {icon}
+          </Icon>
+          <span>{title}</span>
+        </li>
+      )}
     </NavLink>
   );
 }
