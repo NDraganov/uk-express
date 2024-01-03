678cd6303280c5d0633b875441e901c72ae83881
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2024-01-03 02:01:27 +0000

    Add dark mode for the top categories

--- src/features/home/TopCategories.tsx
+++ src/features/home/TopCategories.tsx
@@ -18,46 +18,46 @@ export default function TopCategories() {
   const { data: jewellery } = useGetWomensJewelleryQuery(undefined);
 
   return (
-    <div className="mx-10 my-10">
-      <h2 className="text-xl">Top categories</h2>
-      <div className="grid grid-cols-6 gap-2">
+    <section className="mx-10 my-10">
+      <h2 className="mb-4 text-xl dark:text-white">Top categories</h2>
+      <div className="grid grid-cols-6 gap-2 dark:text-white">
         <NavLink to="/products/category/laptops">
-          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600">
+          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 hover:dark:text-cyan-500">
             <Img className="h-5/6" src={laptops?.products[2].thumbnail} />
-            <p className="dark:text-black">Laptops</p>
+            <p>Laptops</p>
           </div>
         </NavLink>
         <NavLink to="/products/category/mens-shoes">
-          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600">
+          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 hover:dark:text-cyan-500">
             <Img className="h-5/6" src={shoes?.products[4].thumbnail} />
-            <p className="dark:text-black">Shoes</p>
+            <p>Shoes</p>
           </div>
         </NavLink>
         <NavLink to="/products/category/smartphones">
-          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600">
+          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 hover:dark:text-cyan-500">
             <Img className="h-5/6" src={smartphones?.products[1].thumbnail} />
-            <p className="dark:text-black">Smartphones</p>
+            <p>Smartphones</p>
           </div>
         </NavLink>
         <NavLink to="/products/category/womens-dresses">
-          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600">
+          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 hover:dark:text-cyan-500">
             <Img className="h-5/6" src={bags?.products[1].thumbnail} />
-            <p className="dark:text-black">Dresses</p>
+            <p>Dresses</p>
           </div>
         </NavLink>
         <NavLink to="/products/category/mens-watches">
-          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600">
+          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 hover:dark:text-cyan-500">
             <Img className="h-5/6" src={watches?.products[2].thumbnail} />
-            <p className="dark:text-black">Watches</p>
+            <p>Watches</p>
           </div>
         </NavLink>
         <NavLink to="/products/category/womens-jewellery">
-          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600">
+          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 hover:dark:text-cyan-500">
             <Img className="h-5/6" src={jewellery?.products[2].thumbnail} />
-            <p className="dark:text-black">Jewellery</p>
+            <p>Jewellery</p>
           </div>
         </NavLink>
       </div>
-    </div>
+    </section>
   );
 }
