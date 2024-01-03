f36fa669a7bed4f7482c3b52ec7285a8147bdc1b
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2024-01-03 02:09:41 +0000

    Improve the styles for the top categories

--- src/features/home/TopCategories.tsx
+++ src/features/home/TopCategories.tsx
@@ -22,38 +22,56 @@ export default function TopCategories() {
       <h2 className="mb-4 text-xl dark:text-white">Top categories</h2>
       <div className="grid grid-cols-6 gap-2 dark:text-white">
         <NavLink to="/products/category/laptops">
-          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 hover:dark:text-cyan-500">
-            <Img className="h-5/6" src={laptops?.products[2].thumbnail} />
+          <div className="dark:bg-slate-800items-center flex h-36 w-56  flex-col justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 dark:bg-slate-800 hover:dark:text-cyan-500">
+            <Img
+              className="h-5/6 rounded-md"
+              src={laptops?.products[2].thumbnail}
+            />
             <p>Laptops</p>
           </div>
         </NavLink>
         <NavLink to="/products/category/mens-shoes">
-          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 hover:dark:text-cyan-500">
-            <Img className="h-5/6" src={shoes?.products[4].thumbnail} />
+          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 dark:bg-slate-800 hover:dark:text-cyan-500">
+            <Img
+              className="h-5/6 rounded-md"
+              src={shoes?.products[4].thumbnail}
+            />
             <p>Shoes</p>
           </div>
         </NavLink>
         <NavLink to="/products/category/smartphones">
-          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 hover:dark:text-cyan-500">
-            <Img className="h-5/6" src={smartphones?.products[1].thumbnail} />
+          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 dark:bg-slate-800 hover:dark:text-cyan-500">
+            <Img
+              className="h-5/6 rounded-md"
+              src={smartphones?.products[1].thumbnail}
+            />
             <p>Smartphones</p>
           </div>
         </NavLink>
         <NavLink to="/products/category/womens-dresses">
-          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 hover:dark:text-cyan-500">
-            <Img className="h-5/6" src={bags?.products[1].thumbnail} />
+          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 dark:bg-slate-800 hover:dark:text-cyan-500">
+            <Img
+              className="h-5/6 rounded-md"
+              src={bags?.products[1].thumbnail}
+            />
             <p>Dresses</p>
           </div>
         </NavLink>
         <NavLink to="/products/category/mens-watches">
-          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 hover:dark:text-cyan-500">
-            <Img className="h-5/6" src={watches?.products[2].thumbnail} />
+          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 dark:bg-slate-800 hover:dark:text-cyan-500">
+            <Img
+              className="h-5/6 rounded-md"
+              src={watches?.products[2].thumbnail}
+            />
             <p>Watches</p>
           </div>
         </NavLink>
         <NavLink to="/products/category/womens-jewellery">
-          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 hover:dark:text-cyan-500">
-            <Img className="h-5/6" src={jewellery?.products[2].thumbnail} />
+          <div className="flex h-36 w-56 flex-col items-center justify-center gap-2 rounded-md px-5 py-2 shadow-xl hover:scale-105 hover:text-cyan-600 dark:bg-slate-800 hover:dark:text-cyan-500">
+            <Img
+              className="h-5/6 rounded-md"
+              src={jewellery?.products[2].thumbnail}
+            />
             <p>Jewellery</p>
           </div>
         </NavLink>
