d73d196fa69a85660d7d57037854c62e82fb4426
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2024-01-16 00:11:39 +0000

    Add mobile styles for the footer

--- src/features/footer/Footer.tsx
+++ src/features/footer/Footer.tsx
@@ -8,12 +8,14 @@ import FooterNewsLetter from "./footer-components/FooterNewsLetter";
 export default function Footer() {
   return (
     <footer className="flex flex-col justify-between gap-10 border-t border-gray-300 bg-gradient-to-b from-lime-700 to-lime-500 p-4 text-black dark:border-gray-500 dark:bg-slate-900 dark:from-slate-900 dark:to-slate-700 dark:text-gray-400">
-      <div className="flex items-start justify-between">
+      <div className="flex flex-col-reverse items-start justify-between gap-8 sm:flex-row">
         <FooterNewsLetter />
-        <ShopLinks />
-        <LearnLinks />
-        <FollowLinks />
-        <ButtonToTop />
+        <div className="flex w-full items-start justify-between sm:pl-20">
+          <ShopLinks />
+          <LearnLinks />
+          <FollowLinks />
+          <ButtonToTop />
+        </div>
       </div>
       <PoliciesLinks />
     </footer>
