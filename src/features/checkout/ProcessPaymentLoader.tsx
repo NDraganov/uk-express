36ffbe9e68ed6595c789b13cc5862367513582de
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2024-01-23 20:12:05 +0000

    Add mobile styles for the payment loader modal

--- src/features/checkout/ProcessPaymentLoader.tsx
+++ src/features/checkout/ProcessPaymentLoader.tsx
@@ -9,7 +9,7 @@ export default function ProcessPaymentLoader() {
   return (
     <div className="fixed bottom-0 left-0 right-0 top-0 z-20 bg-white/50 dark:bg-white/60">
       <div className="flex h-full items-center justify-center">
-        <div className="flex h-96 w-[40rem] flex-col items-center justify-center gap-7 rounded-md bg-violet-950">
+        <div className="flex h-3/6 w-5/6 flex-col items-center justify-center gap-7 rounded-md bg-violet-950 sm:h-96 sm:w-[40rem]">
           <div className="animate-ping rounded-full bg-pink-500 p-2">
             <div className="rounded-full bg-violet-950 p-4">
               <Icon value={{ className: "text-7xl fill-pink-500" }}>
