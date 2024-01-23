240c4d3caa5982d499258f7bb8426fe61d79dfea
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2024-01-23 14:51:15 +0000

    Add some more mobile styles for the big sale

--- src/features/home/BigSale.tsx
+++ src/features/home/BigSale.tsx
@@ -4,7 +4,7 @@ export default function BigSale() {
   return (
     <section className="my-10 flex items-center justify-center sm:h-[40rem]">
       <div className="flex flex-col items-start justify-center">
-        <h3 className="-rotate-6 rounded-xl border-[1rem] border-rose-600 px-10 py-4 text-5xl font-extrabold text-rose-600 sm:text-9xl">
+        <h3 className="-rotate-6 rounded-xl border-8 border-rose-600 px-10 py-4 text-5xl font-extrabold text-rose-600 sm:border-[1rem] sm:text-9xl">
           BIG Sale!!!
         </h3>
         <div className="">
@@ -13,7 +13,7 @@ export default function BigSale() {
             <span className="text-3xl text-rose-600 sm:text-7xl">TIME</span>{" "}
             only!
           </p>
-          <p className="text-2xl dark:text-white">
+          <p className="dark:text-white sm:text-2xl">
             Till next{" "}
             <span className="font-semibold text-rose-600">FRIDAY</span>
           </p>
--- src/ui/DiscountedButton.tsx
+++ src/ui/DiscountedButton.tsx
@@ -4,7 +4,7 @@ export default function DiscountedButton() {
   return (
     <button className="mt-4 rounded-full px-4 py-2 ring ring-red-600 hover:ring-offset-2 dark:hover:ring-offset-slate-900">
       <NavLink
-        className="text-2xl font-medium text-red-600 hover:font-bold"
+        className="font-medium text-red-600 hover:font-bold sm:text-2xl"
         to="/products-on-sale"
       >
         Products on sale
