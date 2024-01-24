fb42478a35cb1f42882e4f715c1ad6ec7a6c7d6e
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2024-01-24 00:24:18 +0000

    Add close toast functionality

--- src/ui/Toast.tsx
+++ src/ui/Toast.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { useAppDispatch } from "../store/hooks";
+import { closeToast } from "../features/cart/cartSlice";
 import Icon from "./Icon";
 import { AiOutlineClose } from "react-icons/ai";
 import { BsFillCartPlusFill } from "react-icons/bs";
@@ -8,11 +11,19 @@ interface ToastProps {
 }
 
 export default function Toast({ type }: ToastProps) {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch(closeToast());
+    }, 4000);
+  }, [dispatch]);
+
   return (
     <div className="fixed right-10 top-20 z-10 h-12 w-60 rounded-md bg-white shadow-md dark:bg-slate-800 dark:shadow-gray-500 sm:right-20 sm:top-20">
       <div className="flex h-full items-center justify-between p-2">
         <div className="flex items-center gap-2">
-          <div className="flex h-8 w-8 items-center justify-center rounded-md bg-slate-100 dark:bg-slate-700">
+          <div className="flex h-8 w-8 items-center justify-center rounded-md bg-slate-100 dark:bg-slate-600">
             <Icon
               value={{
                 className: `text-2xl ${
@@ -29,7 +40,7 @@ export default function Toast({ type }: ToastProps) {
             {type === "delete" && <p>Product deleted!</p>}
           </div>
         </div>
-        <div>
+        <button onClick={() => dispatch(closeToast())}>
           <Icon
             value={{
               className:
@@ -38,7 +49,7 @@ export default function Toast({ type }: ToastProps) {
           >
             <AiOutlineClose />
           </Icon>
-        </div>
+        </button>
       </div>
     </div>
   );
