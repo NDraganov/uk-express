0a9386af90a3e245f2d920c99e0970031ccfd784
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2024-01-28 00:37:48 +0000

    Add pagination for the products page

--- src/features/categories/all-products/AllProducts.tsx
+++ src/features/categories/all-products/AllProducts.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { useGetAllProductsQuery } from "../../../api/productsApiSlice";
 import ErrorMessage from "../../../ui/ErrorMessage";
 import ProductsList from "../../products/ProductsList";
+import Pagination from "../../../ui/Pagination";
 
 export default function AllProducts() {
+  const [currentPage, setCurrentPage] = useState(1);
   const {
     data: allProducts,
     isLoading,
@@ -27,9 +30,28 @@ export default function AllProducts() {
     }
   }
 
+  const itemsPerPage = 21;
+  const totalPages =
+    allProducts?.products.length &&
+    Math.ceil(allProducts.products.length / itemsPerPage);
+
+  // Get current products
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = allProducts?.products?.slice(
+    indexOfFirstItem,
+    indexOfLastItem,
+  );
+
+  // Change page
+  function paginate(pageNumber: number) {
+    setCurrentPage(pageNumber);
+  }
+
   return (
     <div className="mt-20">
-      <ProductsList data={allProducts?.products} isLoading={isLoading} />
+      <ProductsList data={currentItems} isLoading={isLoading} />
+      <Pagination totalPages={totalPages} paginate={paginate} />
     </div>
   );
 }
--- src/ui/Pagination.tsx
+++ src/ui/Pagination.tsx
@@ -1,26 +1,26 @@
 import { NavLink } from "react-router-dom";
 
 interface PaginationProps {
-  totalItems: number;
-  itemsPerPage: number;
+  totalPages: number | undefined;
+  paginate: (number: number) => void;
 }
 
-export default function Pagination({
-  totalItems,
-  itemsPerPage,
-}: PaginationProps) {
+export default function Pagination({ totalPages, paginate }: PaginationProps) {
   const pageNumbers = [];
 
-  for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
-    pageNumbers.push(i);
-  }
+  if (totalPages !== undefined)
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(i);
+    }
 
   return (
     <nav>
       <ul>
         {pageNumbers.map((number) => (
           <li key={number}>
-            <NavLink to="!#">{number}</NavLink>
+            <NavLink to="!#" onClick={() => paginate(number)}>
+              {number}
+            </NavLink>
           </li>
         ))}
       </ul>
