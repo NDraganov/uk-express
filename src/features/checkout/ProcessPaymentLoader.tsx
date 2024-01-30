ecbc8f94c36e51d85837546de5975716740ed773
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2024-01-30 21:10:53 +0000

    Change styles for the payment loader

--- src/features/checkout/ProcessPaymentLoader.tsx
+++ src/features/checkout/ProcessPaymentLoader.tsx
@@ -9,7 +9,7 @@ export default function ProcessPaymentLoader() {
   return (
     <div className="fixed bottom-0 left-0 right-0 top-0 z-20 bg-white/50 dark:bg-white/60">
       <div className="flex h-full items-center justify-center">
-        <div className="flex h-3/6 w-5/6 flex-col items-center justify-center gap-7 rounded-md bg-violet-950 sm:h-96 sm:w-[40rem]">
+        <div className="flex h-3/6 w-5/6 flex-col items-center justify-end gap-28 rounded-md bg-violet-950 pb-10 sm:h-96 sm:w-[40rem]">
           <div className="animate-ping rounded-full bg-pink-500 p-2">
             <div className="rounded-full bg-violet-950 p-4">
               <Icon value={{ className: "text-7xl fill-pink-500" }}>
@@ -17,7 +17,7 @@ export default function ProcessPaymentLoader() {
               </Icon>
             </div>
           </div>
-          <h1 className="pt-10 text-3xl text-white">
+          <h1 className="pt-10 text-xl text-white sm:text-3xl">
             {isProcessed ? "Payment successful!" : "Processing your payment..."}
           </h1>
         </div>
