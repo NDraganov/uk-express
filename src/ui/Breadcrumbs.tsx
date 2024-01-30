9350745feb57d4009ac0b46f62268f5128961771
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2024-01-30 22:24:26 +0000

    Improve the breadcrumbs styles

--- src/ui/Breadcrumbs.tsx
+++ src/ui/Breadcrumbs.tsx
@@ -17,7 +17,10 @@ export default function Breadcrumbs() {
       const capitalized = crumb.charAt(0).toUpperCase() + crumb.slice(1);
 
       return (
-        <div className="last:text-orange-500" key={crumb}>
+        <div
+          className="last:text-orange-500 dark:last:text-cyan-500"
+          key={crumb}
+        >
           <NavLink className="flex items-center" to={currentLink}>
             <p>{capitalized.replace(/%20|-|_/g, " ")}</p>
             <Icon value={{ className: "text-lg" }}>
@@ -34,11 +37,11 @@ export default function Breadcrumbs() {
 
   return (
     <div className={`${location.pathname === "/" && "hidden"}`}>
-      <div className="invisible ml-10 mt-24 flex items-center gap-2 sm:visible">
+      <div className="invisible ml-10 mt-24 flex items-center gap-2 dark:text-white sm:visible">
         <Icon value={{ className: "text-lg" }}>
           <FaHome />
-          <MdOutlineKeyboardArrowRight />
         </Icon>
+        <MdOutlineKeyboardArrowRight />
         {crumbs}
       </div>
     </div>
