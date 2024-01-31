288c7e7b525f0efeb0920771d6ecef7b3f1d0f0f
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2024-01-31 00:53:03 +0000

    Add conditionally rendering for the results paragraph

--- src/features/products/ProductsList.tsx
+++ src/features/products/ProductsList.tsx
@@ -25,13 +25,25 @@ export default function ProductsList({
     );
   return (
     <div className="mx-4 my-20 md:mx-40">
-      <p className="mb-4 text-2xl font-light dark:text-gray-400">
-        Results <span className="text-lg dark:text-cyan-500">{firstIndex}</span>
-        <span className="text-sm"> to </span>
-        <span className="text-lg dark:text-cyan-500">{lastIndex}</span>
-        <span className="text-sm"> of </span>
-        <span className="text-lg dark:text-cyan-500">{data?.length}</span>
-      </p>
+      {currentData ? (
+        <p className="mb-4 text-2xl font-light dark:text-slate-100">
+          Results{" "}
+          <span className="text-lg dark:text-cyan-500">{firstIndex}</span>
+          <span className="text-sm"> to </span>
+          <span className="text-lg dark:text-cyan-500">{lastIndex}</span>
+          <span className="text-sm"> of </span>
+          <span className="text-lg font-normal dark:text-cyan-500">
+            {data?.length}
+          </span>
+        </p>
+      ) : (
+        <p className="mb-4 text-2xl font-light dark:text-slate-100">
+          Results <span className="text-lg dark:text-cyan-500">1</span>
+          <span className="text-sm dark:text-gray-400"> of </span>
+          <span className="text-lg dark:text-cyan-500">{data?.length}</span>
+        </p>
+      )}
+
       <ul className="grid gap-5 md:grid-cols-3 2xl:grid-cols-4">
         {currentData
           ? currentData?.map((product) => (
