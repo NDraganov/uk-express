8da99dfde7e0f56dda5a5d4c14a3839d55db3252
Nedyalko Draganov <nedyalkodraganov90@gmail.com>
2024-02-14 01:09:04 +0000

    Display the cart modal on full screen on mobile

--- src/features/cart/cart-modal/CartModal.tsx
+++ src/features/cart/cart-modal/CartModal.tsx
@@ -7,7 +7,7 @@ export default function CartModal() {
   return (
     <div className="fixed bottom-0 left-0 right-0 top-0 z-10 bg-black/70 dark:bg-white/30">
       <div
-        className="absolute bottom-0 left-3/4 right-0 top-0 z-10 border border-slate-300 bg-white p-4 text-black shadow-md  dark:border-gray-500 dark:bg-slate-900 dark:text-gray-400 dark:shadow-slate-700"
+        className="absolute bottom-0 right-0 top-0 z-10 border border-slate-300 bg-white p-4 text-black shadow-md dark:border-gray-500  dark:bg-slate-900 dark:text-gray-400 dark:shadow-slate-700 sm:left-3/4"
         onClick={(e: FormEvent) => e.stopPropagation()}
       >
         <div className="flex h-full flex-col justify-between">
